142713b59f99aa0eb2e2a5b1da218551c16db293
ilya-uzun <ilya.uzun@bk.ru>
2021-08-17 20:58:22 +0500

    up

--- app/src/main/java/com/uzun/authorization_android/MainActivity.kt
+++ app/src/main/java/com/uzun/authorization_android/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.uzun.authorization_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn: View = findViewById<View>(R.id.sign_in_button) as View
+
     }
+
+
 }
