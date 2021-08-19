94126a3d6883dbc7e08344d987af3d3b05615a3d
ilya-uzun <ilya.uzun@bk.ru>
2021-08-19 21:37:01 +0500

    ошибка

--- app/src/main/java/com/uzun/authorization_android/MainActivity.kt
+++ app/src/main/java/com/uzun/authorization_android/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.uzun.authorization_android
 
-import android.R
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn: View = findViewById<View>(R.id.sign_in_button) as View
-
     }
-
-
 }
